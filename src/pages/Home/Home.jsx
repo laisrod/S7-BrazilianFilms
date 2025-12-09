08b1b2f236670667d74807d57fd155d8ccc94b3a
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-12-09 14:29:50 -0300

    feat: create Home page component
    - Add Home page as main entry point
    - Integrate MovieList component
    - Add page-specific styling

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { loadMovies } from '../../store/slices/moviesSlice'
+import MovieList from '../../components/MovieList/MovieList'
+import './Home.css'
+
+const Home = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(loadMovies(1))
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <header className="home__header">
+        <h1 className="home__title">Cinema Brasileiro</h1>
+        <p className="home__subtitle">Descubra os grandes clássicos do cinema nacional</p>
+      </header>
+      <main className="home__main">
+        <MovieList />
+      </main>
+    </div>
+  )
+}
+
+export default Home
