82b99d88e784e96be8ec9bd15ef42bf8db4e25e1
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-12-12 11:38:42 -0300

    refactor: convert entry points to TypeScript
    - Convert main.jsx to main.tsx with proper React types
    - Convert App.jsx to App.tsx
    - Add route for MovieDetail page
    - Add proper type assertions for DOM elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import MovieDetail from './pages/MovieDetail/MovieDetail'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
       </Routes>
     </div>
   )
